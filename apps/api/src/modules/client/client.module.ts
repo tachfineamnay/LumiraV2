34c03ef81dafa433114da1e18481bf686cca7923
Tachfine Amnay <tachfine.amnay@gmail.com>
2026-07-17 18:20:01 +0100

    fix(client): register Sanctuaire compatibility boundary

--- apps/api/src/modules/client/client.module.ts
+++ apps/api/src/modules/client/client.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ClientController } from './client.controller';
 import { ClientService } from './client.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { ServicesModule } from '../../services/services.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { ClientSanctuaireInterceptor } from './client-sanctuaire.interceptor';
 
 @Module({
-    imports: [PrismaModule, ServicesModule, NotificationsModule],
-    controllers: [ClientController],
-    providers: [ClientService],
-    exports: [ClientService],
+  imports: [PrismaModule, ServicesModule, NotificationsModule],
+  controllers: [ClientController],
+  providers: [
+    ClientService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClientSanctuaireInterceptor,
+    },
+  ],
+  exports: [ClientService],
 })
-export class ClientModule { }
+export class ClientModule {}
