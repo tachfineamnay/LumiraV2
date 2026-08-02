c65771fe57976f9e2504965dc529ca54ccf26a4b
Tachfine Amnay <tachfine.amnay@gmail.com>
2026-08-02 13:02:23 +0100

    feat(amendments): register transaction facade

--- apps/api/src/modules/reading-amendments/reading-amendment.module.ts
+++ apps/api/src/modules/reading-amendments/reading-amendment.module.ts
@@ -6,12 +6,13 @@ import {
   ClientReadingAmendmentController,
   ExpertReadingAmendmentController,
 } from './reading-amendment.controller';
+import { ReadingAmendmentFacade } from './reading-amendment.facade';
 import { ReadingAmendmentService } from './reading-amendment.service';
 
 @Module({
   imports: [PrismaModule, UploadsModule, ExpertModule],
   controllers: [ClientReadingAmendmentController, ExpertReadingAmendmentController],
-  providers: [ReadingAmendmentService],
-  exports: [ReadingAmendmentService],
+  providers: [ReadingAmendmentService, ReadingAmendmentFacade],
+  exports: [ReadingAmendmentService, ReadingAmendmentFacade],
 })
 export class ReadingAmendmentModule {}
