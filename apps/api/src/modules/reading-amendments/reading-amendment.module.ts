1740f3fa0b7cdfb3059bf04280a657f143c25372
Tachfine Amnay <tachfine.amnay@gmail.com>
2026-08-02 13:36:09 +0100

    refactor(api): keep amendment core service private

--- apps/api/src/modules/reading-amendments/reading-amendment.module.ts
+++ apps/api/src/modules/reading-amendments/reading-amendment.module.ts
@@ -14,6 +14,6 @@ import { ReadingAmendmentService } from './reading-amendment.service';
   imports: [PrismaModule, UploadsModule, ExpertModule, NotificationsModule],
   controllers: [ClientReadingAmendmentController, ExpertReadingAmendmentController],
   providers: [ReadingAmendmentService, ReadingAmendmentFacade],
-  exports: [ReadingAmendmentService, ReadingAmendmentFacade],
+  exports: [ReadingAmendmentFacade],
 })
 export class ReadingAmendmentModule {}
